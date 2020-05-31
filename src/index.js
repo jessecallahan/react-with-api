3e4aa8eb532a6f5505ca0c0c79affa7016fb43a6
Jesse Callahan <jessetylercallahan@gmail.com>
2020-05-31 10:48:42 -0700

    goes through api, middleware, thunk lesson

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
+
+
+import reducer from './reducers/headlines-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
